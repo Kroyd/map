95f5264e5788a66258522a6e52fa764b41747f50
Kroyd <kroydjaykven@gmail.com>
2023-05-23 11:24:29 +0500

    Display distance in meters

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <l-map style="height: 300px" :zoom="zoom" :center="center">
+<div>
+  <l-map style="height: 100vh" ref="map" :zoom="zoom" :center="center"
+   @update:zoom="zoomUpdated"
+   @update:center="centerUpdated">
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-marker :lat-lng="markerLatLng"></l-marker>
+    <l-marker :lat-lng="markerLatLng1">
+    <l-popup> {{ markerLatLng1 }}</l-popup>
+    </l-marker>
+    <l-marker :lat-lng="markerLatLng2">
+    <l-popup>{{ markerLatLng2 }}</l-popup>
+    </l-marker>
+    <l-marker :lat-lng="markerLatLng3">
+    <l-popup>{{ markerLatLng3 }}</l-popup>
+    </l-marker>
+    <l-polygon :lat-lngs="polygon"></l-polygon>
   </l-map>
+  <button @click="calculateDistance">Расстояние</button> <br>
+  <div v-if="distance">Расстояние: {{ distance }}</div> <br>
+</div>
 </template>
 
 <script>
-import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
-
+import {LMap, LTileLayer, LMarker, LPopup,LPolygon} from 'vue2-leaflet';
+import L from 'leaflet'
 export default {
   components: {
     LMap,
     LTileLayer,
-    LMarker
+    LMarker,
+    LPopup,
+    LPolygon,
   },
   data () {
     return {
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
         '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 15,
-      center: [51.505, -0.159],
-      markerLatLng: [51.504, -0.159]
+      zoom: 12,
+      center: [40.28466566765378, 69.62443828582765],
+      markerLatLng1: [40.308783, 69.650615],
+      markerLatLng2: [40.278352, 69.628042],
+      markerLatLng3: [40.302086, 69.614105],
+      polygon: [[40.308783, 69.650615], [40.278352, 69.628042]],
+      distance: null,
     };
-  }
+  },
+  methods: {
+    zoomUpdated (zoom) {
+      this.zoom = zoom
+    },
+    centerUpdated ( center ) {
+      this.center = center
+    },
+    calculateDistance() {
+      let officeStadium = (L.latLng(40.278352, 69.628042));
+      let office34 = (L.latLng(40.308783, 69.650615));
+
+      const distance =  officeStadium.distanceTo(office34).toFixed(2);
+      
+      this.distance = distance
+    }
+  },
 }
 </script>
