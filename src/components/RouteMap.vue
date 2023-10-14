35c88efd2cc346054201c350920e24dd267f8227
khusrav <kroydjaykven@gmail.com>
2023-10-14 18:20:54 +0500

    bag

--- src/components/RouteMap.vue
+++ src/components/RouteMap.vue
@@ -15,6 +15,8 @@
       </l-marker>
       <l-polygon :lat-lngs="polygon"></l-polygon>
     </l-map>
+    <button @click="getClick">Давай</button>
+    <button @click="getClick2">Отмена</button>
     <button @click="calculateDistance">Расстояние</button> <br>
     <div v-if="distance">Расстояние: {{ distance }}</div> <br>
   </div>
@@ -44,19 +46,80 @@
         markerLatLng3: [40.302086, 69.614105],
         polygon: [[40.308783, 69.650615], [40.278352, 69.628042]],
         distance: null,
-  
+        routing: null,
+        home: require('@/assets/svg/home.svg'),
+        map: null
       };
     },
     mounted() {
+      //       var thisIcon = new L.Icon({
+//     iconUrl:  require('@/assets/svg/home.svg'),
+//     iconAnchor: new L.Point(24, 24),
+// });
+//   var icon = new L.Icon({
+//     iconUrl:  require('@/assets/svg/home.svg'),
+//     iconAnchor: new L.Point(1, 1),
+// });
       const map = this.$refs.map.mapObject;
       L.Routing.control({
+        // show: false,
+        // addWaypoints: false,
+        // draggableWaypoints: false,
+        // fitSelectedRoutes: false,
         waypoints: [
       L.latLng(40.308783, 69.650615),
       L.latLng(40.278352, 69.628042),
-     ]
+     ],
+      //  createMarker: function () { return null; },
+    //  createMarker: function (i, wp) {
+    //   if (i === 0) {
+    //     return null
+    //     // First marker
+    //     // return L.marker(wp.latLng, {
+    //     //   icon: thisIcon,
+    //     //   // icon: blankMarker,
+    //     // });
+    //   } else {
+    //     // Last marker
+    //     // return null
+    //     return L.marker(wp.latLng, {
+    //       icon: thisIcon,
+    //       // icon: bMarker,
+    //       // icon: require('@/assets/svg/home.svg')
+    //     });
+    //   }
+    // },
+    //  show: false,
+    // addWaypoints: false,
+    // draggableWaypoints: false,
+    // fitSelectedRoutes: false,
+    lineOptions: {
+      styles: [{ color: "blue", opacity: 0.7, weight: 3 }],
+    },
+    // show: this.getShow,
+    // hide: function() {
+		// 	L.DomUtil.addClass(this._container, 'leaflet-routing-container-hide');
+		// },
       }).addTo(map)
     },
     methods: {
+      getClick() {
+        this.routing = L.Routing.control({
+          waypoints: [
+      L.latLng(40.308783, 69.650615),
+      L.latLng(40.278352, 69.628042),
+     ],
+    //  lineOptions: {
+    //   styles: [{ color: "blue", opacity: 0.7, weight: 3 }],
+    // },
+  }).addTo(this.map)
+    },
+    getClick2() {
+        this.routing.spliceWaypoints(1, 1);
+      },
+      // getShow() {
+      //   L.DomUtil.removeClass(this._container, 'leaflet-routing-container-hide');
+      // },
       zoomUpdated (zoom) {
         this.zoom = zoom
       },
@@ -74,6 +137,12 @@
     },
   }
   </script>
+
+<style>
+.leaflet-routing-container-hide {
+  display: none;
+}
+</style>
 
 
 <!-- <template>
